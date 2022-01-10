42017a03b9be31831d859644d3fa83964624e7f6
samyuktha1 <55914432+samyuktha1@users.noreply.github.com>
2022-01-10 14:24:31 +0530

    Update functionParameter.js

--- functionParameter.js
+++ functionParameter.js
@@ -9,15 +9,3 @@ function sum1(sum,z)
 var add=sum(5,7);
 var third=sum1(add,3);
 console.log(third)
-
-function sum(x,y)
-{
-    return x+y;
-}
-function sum1(callbackFunc,z)
-{
-    return callbackFunc+z;
-}
-var add=sum(5,7);
-var third=sum1(add,3);
-console.log(third)
