f04cad4f5123c530ffd7ed84ae4096ebf813f30f
samyuktha1 <55914432+samyuktha1@users.noreply.github.com>
2022-01-10 14:25:06 +0530

    Create ES6.js

---
+++ ES6.js
@@ -0,0 +1,14 @@
+
+const printName =(name)=>"Hi"+name;
+//console.log(printName("samyu"))
+
+const printBill=(name,bill)=>"Hey "+name+" Please pay"+"$"+bill;
+//console.log(printBill("Samyu",200))
+
+const person={
+    name: "Noam Chomsky",
+    age: 92
+}
+const{name,age}=person;
+console.log(name);
+console.log(age);
